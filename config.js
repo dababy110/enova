012bc0ddfedd9f12ab77ef8b5e3672e807435ff1
Dababy110 <vargasariasdavid110@gmail.com>
2024-08-16 00:08:25 -0600

    Upgrades

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-export const API_KEY_CHAT_ENOVA = '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';
+export const API_KEY_OPENAI = 'API_KEY';
 
-export const API_KEY_IMAGE_DETECTION = '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';
-
-export const API_KEY_GOOGLE_VISION = 'AIzaSyBN-Z5Ai2wsXYHMPqLnAJSfLB56jJOfK5I';
+export const API_KEY_GOOGLE_VISION = 'API_KEY';
